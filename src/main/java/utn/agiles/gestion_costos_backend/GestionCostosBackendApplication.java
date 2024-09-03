879c4119077f0981dd9c932bdebf138732562811
JoseLlontop <josellontop100@gmail.com>
2024-09-03 16:20:33 -0300

    Estructura base del backend creada con Spring Boot

---
+++ src/main/java/utn/agiles/gestion_costos_backend/GestionCostosBackendApplication.java
@@ -0,0 +1,13 @@
+package utn.agiles.gestion_costos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCostosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCostosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utn/agiles/gestion_costos_backend/GestionCostosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.agiles.gestion_costos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCostosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
