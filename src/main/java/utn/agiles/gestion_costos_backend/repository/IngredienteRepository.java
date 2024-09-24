309adee0282bbf421b25fe85d1fd99dddc41c0aa
JoseLlontop <josellontop100@gmail.com>
2024-09-24 17:27:01 -0300

    Se agrego la clase que maneja la logica relacionada con la base de datos

--- src/main/java/utn/agiles/gestion_costos_backend/repository/IngredienteRepository.java
+++ src/main/java/utn/agiles/gestion_costos_backend/repository/IngredienteRepository.java
@@ -1,2 +1,9 @@
-package utn.agiles.gestion_costos_backend.repository;public interface IngredienteRepository {
+package utn.agiles.gestion_costos_backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import utn.agiles.gestion_costos_backend.models.Ingrediente;
+
+@Repository
+public interface IngredienteRepository extends CrudRepository<Ingrediente, Integer> {
 }
