cacf9b9218db7ed51e9f67347c0cbf0ea8e8c7b6
JoseLlontop <josellontop100@gmail.com>
2024-09-24 17:25:43 -0300

    Se agrego la entidad Ingrediente con sus atributos

--- src/main/java/utn/agiles/gestion_costos_backend/models/Ingrediente.java
+++ src/main/java/utn/agiles/gestion_costos_backend/models/Ingrediente.java
@@ -1,2 +1,31 @@
-package utn.agiles.gestion_costos_backend.models;public class Ingrediente {
+package utn.agiles.gestion_costos_backend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@Table(name = "ingrediente")
+public class Ingrediente {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "costo_unidad")
+    private float costo_unidad;
+
+    @Column(name = "unidad_medida")
+    private String unidad_medida;
+
+    @Column(name = "stock")
+    private int stock;
+
 }
+
+
