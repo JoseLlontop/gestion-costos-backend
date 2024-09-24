94e0d10eee037135bfe4c96a7acb6ba2b3a1a92b
JoseLlontop <josellontop100@gmail.com>
2024-09-24 17:32:28 -0300

    Se agrego la clase que maneja las CORS y permite aceptar peticiones de un proyecto en React

--- src/main/java/utn/agiles/gestion_costos_backend/WebConfig.java
+++ src/main/java/utn/agiles/gestion_costos_backend/WebConfig.java
@@ -1,2 +1,17 @@
-package utn.agiles.gestion_costos_backend;public class WebConfig {
+package utn.agiles.gestion_costos_backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173") // URL del origen de la aplicación front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
