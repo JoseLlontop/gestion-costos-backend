1d5d27ae7e7801cc5179129a3c668bc326e8f7af
JoseLlontop <josellontop100@gmail.com>
2024-09-24 17:28:53 -0300

    Se agrego la interface de service para poder realizar inyeccion de dependencias

--- src/main/java/utn/agiles/gestion_costos_backend/services/interfaces/IIngredienteService.java
+++ src/main/java/utn/agiles/gestion_costos_backend/services/interfaces/IIngredienteService.java
@@ -1,2 +1,10 @@
-package utn.agiles.gestion_costos_backend.services.interfaces;public interface IIngredienteService {
+package utn.agiles.gestion_costos_backend.services.interfaces;
+
+import utn.agiles.gestion_costos_backend.models.Ingrediente;
+
+import java.util.List;
+
+public interface IIngredienteService {
+
+    List<Ingrediente> getIngredientes();
 }
