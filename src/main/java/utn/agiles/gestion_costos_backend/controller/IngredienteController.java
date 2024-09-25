eebb5e13302b617f5dfb97e3167d215c98372701
Manuel <manuelscianca01@gmail.com>
2024-09-25 11:36:38 -0300

    implemento el CRUD de ingrediente

--- src/main/java/utn/agiles/gestion_costos_backend/controller/IngredienteController.java
+++ src/main/java/utn/agiles/gestion_costos_backend/controller/IngredienteController.java
@@ -1,9 +1,12 @@
 package utn.agiles.gestion_costos_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utn.agiles.gestion_costos_backend.model.IngredienteModel;
 import utn.agiles.gestion_costos_backend.service.IngredienteService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ingrediente")
@@ -17,5 +20,29 @@ public class IngredienteController {
         return this.ingredienteService.crearIngrediente(ingrediente);
     }
 
+    @GetMapping
+    public List<IngredienteModel> obtenerTodosLosIngredientes() {
+        return ingredienteService.obtenerTodosLosIngredientes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<IngredienteModel> obtenerIngredientePorId(@PathVariable Long id) {
+        return ingredienteService.obtenerIngredientePorId(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<IngredienteModel> actualizarIngrediente(@PathVariable Long id, @RequestBody IngredienteModel detallesIngrediente) {
+        return ingredienteService.actualizarIngrediente(id, detallesIngrediente) != null
+                ? ResponseEntity.ok(detallesIngrediente)
+                : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarIngrediente(@PathVariable Long id) {
+        ingredienteService.eliminarIngrediente(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/utn/agiles/gestion_costos_backend/service/IngredienteService.java
+++ src/main/java/utn/agiles/gestion_costos_backend/service/IngredienteService.java
@@ -5,13 +5,38 @@ import org.springframework.stereotype.Service;
 import utn.agiles.gestion_costos_backend.model.IngredienteModel;
 import utn.agiles.gestion_costos_backend.repository.IIngredienteRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class IngredienteService {
 
     @Autowired
     IIngredienteRepository ingredienteRepository;
 
+    public List<IngredienteModel> obtenerTodosLosIngredientes() {
+        return ingredienteRepository.findAll();
+    }
+
+    public Optional<IngredienteModel> obtenerIngredientePorId(Long id) {
+        return ingredienteRepository.findById(id);
+    }
+
     public IngredienteModel crearIngrediente(IngredienteModel ingrediente){
         return ingredienteRepository.save(ingrediente);
+    }
+
+    public IngredienteModel actualizarIngrediente(Long id, IngredienteModel detallesIngrediente) {
+        return ingredienteRepository.findById(id)
+                .map(ingrediente -> {
+                    ingrediente.setNombre(detallesIngrediente.getNombre());
+                    ingrediente.setUnidad(detallesIngrediente.getUnidad());
+                    ingrediente.setPrecio(detallesIngrediente.getPrecio());
+                    return ingredienteRepository.save(ingrediente);
+                }).orElseThrow(() -> new RuntimeException("Ingrediente no encontrado"));
+    }
+
+    public void eliminarIngrediente(Long id) {
+        ingredienteRepository.deleteById(id);
     }
 }
