b0461e2d9960eb6a51d1fe16b2ad360c53eaaccf
Franco <francodasilva_@outlook.com>
2024-10-01 11:09:09 -0300

    Modificaciones para usar variables de entorno

--- src/main/java/utn/agiles/gestion_costos_backend/GestionCostosBackendApplication.java
+++ src/main/java/utn/agiles/gestion_costos_backend/GestionCostosBackendApplication.java
@@ -1,13 +1,19 @@
 package utn.agiles.gestion_costos_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestionCostosBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionCostosBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Carga las variables de entorno desde el archivo .env
+        Dotenv dotenv = Dotenv.configure().load();
 
+        // Establece las variables de entorno en el sistema
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(GestionCostosBackendApplication.class, args);
+    }
 }
