61321fff9f63cf3f3a3b8384c23e48ffe14dd6a0
Franco <francodasilva_@outlook.com>
2024-10-03 01:27:37 -0300

    Agrego metodo en el servicio IngredienteXRecetaServices para agegar un ingrediente a una recetas

--- src/main/java/utn/agiles/gestion_costos_backend/services/IngredienteXRecetaServices.java
+++ src/main/java/utn/agiles/gestion_costos_backend/services/IngredienteXRecetaServices.java
@@ -1,46 +1,58 @@
 package utn.agiles.gestion_costos_backend.services;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import utn.agiles.gestion_costos_backend.models.*;
+
+import utn.agiles.gestion_costos_backend.models.IngredienteXRecetaId;
+import utn.agiles.gestion_costos_backend.models.IngredienteXRecetaModel;
+import utn.agiles.gestion_costos_backend.models.RecetaModel;
 import utn.agiles.gestion_costos_backend.repository.IIngredienteXRecetaRepository;
 import utn.agiles.gestion_costos_backend.repository.IRecetaRepository;
-
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class IngredienteXRecetaServices {
+
     @Autowired
     private IIngredienteXRecetaRepository ingredienteXRecetaRepository;
-
-    public IngredienteXRecetaModel createIngredienteXReceta( IngredienteXRecetaModel ingredienteXReceta) {
-        
-        if (ingredienteXReceta.getId() == null) {
-            IngredienteXRecetaId id = new IngredienteXRecetaId(
-                ingredienteXReceta.getReceta().getId(),
-                ingredienteXReceta.getIngrediente().getId()
-            );
-            ingredienteXReceta.setId(id);
-        }
-        
-        return ingredienteXRecetaRepository.save(ingredienteXReceta);
-
-    } //* falta manejo de errores */
-
-
-    //! tengo que hacer el repository de IngredienteXRecetaId para obtener el id con las dos foraneas y luego usarlo
     
-    public boolean deleteIngredienteXReceta (Long recetaId , Long ingredienteId){
-        IngredienteXRecetaId id = new IngredienteXRecetaId(recetaId , ingredienteId);
-        if (ingredienteXRecetaRepository.existsById(id)){
+    @Autowired
+    private IRecetaRepository recetaRepository; 
+    
+    public IngredienteXRecetaModel createIngredienteXReceta(IngredienteXRecetaModel ingredienteXRecetaModel) {
+        IngredienteXRecetaId id = new IngredienteXRecetaId(
+            ingredienteXRecetaModel.getReceta().getId(),
+            ingredienteXRecetaModel.getIngrediente().getId()
+        );
+        ingredienteXRecetaModel.setId(id);
+        ingredienteXRecetaModel.calcularCosto();
+        return ingredienteXRecetaRepository.save(ingredienteXRecetaModel);
+    }
+
+    public boolean deleteIngredienteXReceta(Long recetaId, Long ingredienteId) {
+        IngredienteXRecetaId id = new IngredienteXRecetaId(recetaId, ingredienteId);
+        if (ingredienteXRecetaRepository.existsById(id)) {
             ingredienteXRecetaRepository.deleteById(id);
             return true;
         } else {
             return false;
         }
     }
-   
+
+   public IngredienteXRecetaModel addIngredienteToReceta(Long recetaId, Long ingredienteId, float cantidad) {
+    IngredienteXRecetaId id = new IngredienteXRecetaId(recetaId, ingredienteId);
+    IngredienteXRecetaModel ingredienteXReceta = new IngredienteXRecetaModel();
+    ingredienteXReceta.setId(id);
+    ingredienteXReceta.setCantidad(cantidad);
     
+    ingredienteXReceta.calcularCosto();
+
+    RecetaModel receta = recetaRepository.findById(recetaId).orElseThrow(() -> new RuntimeException("Receta no encontrada"));
+    receta.getIngredientes().add(ingredienteXReceta); 
+    ingredienteXRecetaRepository.save(ingredienteXReceta);
+
+    receta.calcularCostoTotal(); 
+    recetaRepository.save(receta);
+    return ingredienteXReceta;
 }
+
+
+} 
