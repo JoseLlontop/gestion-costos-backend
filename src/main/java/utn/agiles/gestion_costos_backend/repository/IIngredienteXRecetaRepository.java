d29fa0e508e87bfdb1db219ef3b8419d38742759
Franco <francodasilva_@outlook.com>
2024-10-03 01:31:36 -0300

    Metodo para busar recetas por id en repository IngredienteXRecetaRepository

--- src/main/java/utn/agiles/gestion_costos_backend/repository/IIngredienteXRecetaRepository.java
+++ src/main/java/utn/agiles/gestion_costos_backend/repository/IIngredienteXRecetaRepository.java
@@ -2,12 +2,12 @@ package utn.agiles.gestion_costos_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import utn.agiles.gestion_costos_backend.models.IngredienteXRecetaId;
 import utn.agiles.gestion_costos_backend.models.IngredienteXRecetaModel;
 
+import java.util.List;
 
 @Repository
-public interface IIngredienteXRecetaRepository extends JpaRepository<IngredienteXRecetaModel , IngredienteXRecetaId >{
-    
+public interface IIngredienteXRecetaRepository extends JpaRepository<IngredienteXRecetaModel, IngredienteXRecetaId> {
+    List<IngredienteXRecetaModel> findByRecetaId(Long recetaId);
 }
