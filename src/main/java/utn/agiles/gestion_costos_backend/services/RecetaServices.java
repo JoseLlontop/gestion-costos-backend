02bab31dd9773a49b10ce9c281e67bca2e99a8cc
Franco <francodasilva_@outlook.com>
2024-10-03 01:46:10 -0300

    Fix RecetaService

--- src/main/java/utn/agiles/gestion_costos_backend/services/RecetaServices.java
+++ src/main/java/utn/agiles/gestion_costos_backend/services/RecetaServices.java
@@ -2,7 +2,7 @@ package utn.agiles.gestion_costos_backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import utn.agiles.gestion_costos_backend.models.*;
+import utn.agiles.gestion_costos_backend.models.RecetaModel;
 import utn.agiles.gestion_costos_backend.repository.IRecetaRepository;
 
 import java.util.List;
@@ -10,8 +10,9 @@ import java.util.Optional;
 
 @Service
 public class RecetaServices {
+    
     @Autowired
-    IRecetaRepository recetaRepository;
+    private IRecetaRepository recetaRepository;
 
     public List<RecetaModel> getRecetas() {
         return recetaRepository.findAll();
@@ -21,27 +22,22 @@ public class RecetaServices {
         return recetaRepository.findById(id);
     }
 
-    public RecetaModel createReceta(RecetaModel receta){
+    public RecetaModel createReceta(RecetaModel receta) {
         return recetaRepository.save(receta);
     }
 
-    public RecetaModel updateReceta(Long id, RecetaModel detallesReceta){
+    public RecetaModel updateReceta(Long id, RecetaModel detallesReceta) {
         return recetaRepository.findById(id).map(receta -> {
             receta.setNombreReceta(detallesReceta.getNombreReceta());
             receta.setDescripcion(detallesReceta.getDescripcion());
-            receta.setCantidadPorciones(detallesReceta.getCantidadPorciones());
-            receta.setCostoTotal(detallesReceta.getCostoTotal());
-            receta.setCostoPorcion(detallesReceta.getCostoPorcion());
+            receta.setPorcionesRinde(detallesReceta.getPorcionesRinde());
+            
             return recetaRepository.save(receta);
-        
         }).orElseThrow(() -> new RuntimeException("Receta no encontrada"));
     }
-
-
 
 
     public void deleteReceta(Long id) {
         recetaRepository.deleteById(id);
     }
-    
 }
